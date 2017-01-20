9a07d05cf6988a8920422ae22f2424a75376d49e
Mel Booth <mel.m.booth@gmail.com>
2017-01-20 16:40:34 +1300

    create list and list-header components

---
+++ src/components/list.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListHeader from './list-header'
+
+export default class List extends React.Component {
+  render() {
+    return (
+      <table>
+        <ListHeader />
+      </table>
+    )
+  }
+}
