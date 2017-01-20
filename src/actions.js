cd3446b8bc200379599f3a54ee789c11894d0d01
Mel Booth <mel.m.booth@gmail.com>
2017-01-20 17:52:12 +1300

    create actions.js - list actions

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+export const ADD_ITEM = 'ADD_ITEM'
+export const TOGGLE_ITEM = 'TOGGLE_ITEM'
+export const SET_FILTER = 'SET_FILTER'
+
+export const filters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETE: 'SHOW_COMPLETE',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
