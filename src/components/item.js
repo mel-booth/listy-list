9a513ba662af775953593d8bc823b0c487489551
Mel Booth <mel.m.booth@gmail.com>
2017-01-20 18:36:35 +1300

    learning about prop types!

--- src/components/item.js
+++ src/components/item.js
@@ -5,3 +5,11 @@ const Item = ({onClick, complete, text}) => (
     {text}
   </li>
 )
+
+Item.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  complete: PropTypes.bool.isREquired,
+  text: PropTypes.string.isRequired
+}
+
+export default Item
