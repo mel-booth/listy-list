246629af457030388b280b1b15efb5767e91e31c
Mel Booth <mel.m.booth@gmail.com>
2017-01-20 16:12:50 +1300

    set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    'webpack-dev-server/client?http://127.0.0.1:8080/',
+    'webpack/hot/only-dev-server',
+    './src'
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test:/\.jsx?$/,
+        exclude: /node_module/,
+        loader: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
